a15fa949d1f6695be66640d402e1f5aa531b8923
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-04-24 22:47:25 +0300

    router + promeni po app.js

--- MovielSite/js/app.js
+++ MovielSite/js/app.js
@@ -1,22 +1,25 @@
 /*jshint esversion: 6 */
-import Navigo from 'navigo';
-import { controller } from 'controller';
-import {userController} from 'user-controller';
+import { MyRouter } from 'router';
+// import * as homeController from 'homeController';
+// import * as myCookieController from 'myCookieController';
+// import * as userController from 'userController';
 
-let router = new Navigo(null, true);
+let router = new MyRouter();
 
 router.
-	on('login', userController.login).
-	on('about', controller.showAbout).
-	on('register', userController.register).
-	on('logout', userController.logout).
-	on('user-panel', controller.showUserPanel).
-	on('posts/:id', (params) => controller.showPostByID(params)).
-	on('posts', (params) => controller.postWorking(params)).
-	on('user/:userID', (params) => controller.showUserPosts(params)).
-	on('home', controller.home).
-	on(() => {
-		router.navigate('/home');
-	}).
-	resolve();
+on('login', userController.login).
+on('about', controller.showAbout).
+on('register', userController.register).
+on('logout', userController.logout).
+on('user-panel', controller.showUserPanel).
+on('posts/:id', (params) => controller.showPostByID(params)).
+on('posts', (params) => controller.postWorking(params)).
+on('user/:userID', (params) => controller.showUserPosts(params)).
+on('home', controller.home).
+on(() => {
+    router.navigate('/home');
+})
 
+
+$(window).on('load', () => router.navigate());
+$(window).on('hashchange', () => router.navigate());
---
+++ MovielSite/js/router.js
@@ -0,0 +1,63 @@
+class MyRouter {
+    constructor() {
+        //we use array to push all url that we have
+        this._routes = [];
+    }
+
+    on(url, callback) {
+        //push the url and the callback in the array;
+        //return this gives the ability to chane
+        this._routes.push({ url, callback });
+        retrun this;
+    }
+
+    navigate() {
+        //this removes the # from the url 
+        const currentUrl = location.hash.slice(1);
+
+
+        for (const { taregetUrl, callback }
+            of this._routes) {
+            const params = MyRouter.matchUrls(currentUrl, targetUrl);
+            if (params) {
+                callback(params);
+                break;
+            }
+        }
+    }
+
+    //check if onUrl and the current url match 
+    static matchUrls(currentUrl, targetUrl) {
+        //split currentUrl on /
+        const currentUrlWords = currentUrl.split(/\//g);
+        //split targetUrl on /
+        const targetUrlWord = targetUrl.split(/\//g);
+
+        //if target and current are with differen length return false -> no match
+        if (targetUrl.length !== currentUrl.length) {
+            return false;
+        }
+
+        //params wich we need 
+        const params = {};
+        const len = currentUrlWords.length;
+
+        //we need to check every word from both target and current url for match 
+        //if all match we will return params if any else will return empty object 
+        for (let i = 0; i < len; i += 1) {
+            if (targetUrlWord[i][0] !== ':') {
+                if (currentUrlWords[i] !== targetUrlWord[i]) {
+                    return false;
+                }
+            } else {
+                const paramName = targetUrlParts[i].slice(1);
+                params[paramName] = currentUrlParts[i];
+            }
+        }
+
+        return params;
+
+    }
+}
+
+export { MyRouter };
