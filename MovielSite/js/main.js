bfec3d0e8b22ac06f7d53712ab23bf54cd4c9e79
tsvetelina- <tsvetelina.a.marinova@gmail.com>
2017-04-29 02:52:44 +0300

    Fixed functionality added templates

--- MovielSite/js/main.js
+++ MovielSite/js/main.js
@@ -3,14 +3,18 @@ import { MyRouter } from './router.js';
 import { DataTransferManager } from "./data.js";
 import { MainController } from "./mainController.js";
 import { ClosestCinema } from "./getClosestCinema.js";
+import { templatesLoader } from "./template-loader.js";
 // import * as homeController from 'homeController';
 // import * as myCookieController from 'myCookieController';
 // import * as userController from 'userController';
 
+// istances
 let router = new MyRouter();
 let data = new DataTransferManager();
 let controller = new MainController(data);
-let searchCinema = new ClosestCinema();
+
+// selectors
+let $contentDiv = $('#content-container');
 
 router
 // .on('login', userController.login)
@@ -21,8 +25,27 @@ router
 // .on('posts/:id', (params) => controller.showPostByID(params))
 // .on('posts', (params) => controller.postWorking(params))
 // .on('user/:userID', (params) => controller.showUserPosts(params))
+    .on('home', function() {
+        $contentDiv.text('');
+    })
     .on('watch', function() {
+        $contentDiv.html('<iframe id="inner-site" src="" frameborder="0"></iframe>' +
+            '<div id="contacts"><span id="location">Our location on map</span></div>' +
+            '<iframe id="right-site" src="" frameborder="0"></iframe>');
+        let searchCinema = new ClosestCinema();
         searchCinema.fIndClosestCinema();
+    })
+    .on('login', function() {
+        templatesLoader.get('login')
+            .then(template => {
+                $contentDiv.html(template());
+            })
+    })
+    .on("register", function() {
+        templatesLoader.get('register')
+            .then(template => {
+                $contentDiv.html(template());
+            })
     })
     .on(() => {
         router.navigate('/home');
