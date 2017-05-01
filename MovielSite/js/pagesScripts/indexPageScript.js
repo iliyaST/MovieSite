13919874618be288e108530992f11a43e78bb894
iliyaST <iliamartinovstambolov@gmail.com>
2017-05-01 12:56:09 +0300

    bug fixing

--- MovielSite/js/pagesScripts/indexPageScript.js
+++
@@ -1,9 +0,0 @@
-$("#navbar").on("click", "a", function() {
-    var parent = $(this).parent().parent();
-    var children = $(parent).find("a");
-    var lenght = children.length;
-    for (var i = 0; i < lenght; i += 1) {
-        $(children[i]).removeClass("clickedNavBar");
-    }
-    $(this).addClass("clickedNavBar");
-});
